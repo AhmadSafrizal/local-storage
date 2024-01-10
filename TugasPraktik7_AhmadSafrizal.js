5fbe4ce17f97a06d79dc37d6a7cfdaf0ff12f567
unknown <ahmadsafrizal01@gmail.com>
2024-01-10 16:17:37 +0700

    first commit

---
+++ TugasPraktik7_AhmadSafrizal.js
@@ -0,0 +1,9 @@
+for (i = 1; i < 16; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("gamelab indonesia");
+  } else if (i % 3 === 0) {
+    console.log("game");
+  } else if (i % 5 === 0) {
+    console.log("gamelab");
+  }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+var x = 7;
+x = Math.pow(7, 2);
+
+console.log(`x = ${x}`);
